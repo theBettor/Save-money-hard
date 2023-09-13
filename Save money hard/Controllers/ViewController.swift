a833355bd0b49ea785842b407565305a80bb4695
theBettor <be0128st@gmail.com>
2023-09-13 17:35:43 +0900

    Initial Commit

---
+++ Save money hard/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Save money hard
+//
+//  Created by 김찬교 on 2023/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
